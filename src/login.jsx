62d0ca0f0ac96b36868bd5507369e052969c37d4
yousef abdallah <yousef.abdallah.lbx@gmail.com>
2025-10-26 23:42:33 +0200

    version 2

--- src/login.jsx
+++ src/login.jsx
@@ -4,24 +4,15 @@ import Skipped_loggin_page from "./skipped_login_page";
 
 
 
-function login(){
 
-    document.getElementById("loggin_page").style.display = none;
-    return <Logged_in_page/>;
-}
-
-function skip(){
-    document.getElementById("loggin_page").style.display = none;
-    return <Skipped_loggin_page/>;
-}
 
 function Login(){
     
     return(
         <section id="loggin_page">
             <h1>Login or skip</h1>
-            <button onClick={login}>Login</button>
-            <button onClick={skip}>Skip</button>
+            <button>Login</button>
+            <button>Skip</button>
         </section>
     )
 
